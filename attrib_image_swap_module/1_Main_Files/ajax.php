b6b5ef8f4255de8f15518787b933f87f9168247d
mc12345678 <mc12345678@users.noreply.github.com>
2025-06-25 18:11:42 -0400

    Update ajax.php to resolve issue
    Modified this version of ajax.php to help update older sites (if they
    choose to incorporate) and to resolve the issue introduced at:
    https://github.com/zencart/zencart/pull/6652
    
    Seems nothing would actually need to be revised otherwise for continued
    operation if the appropriate code modification was incorporated into
    the admin file and the language information was sufficiently/properly
    carried over for ZC 2.x...
    
    ZC 2.1.0 related code to follow after this commit.

--- attrib_image_swap_module/1_Main_Files/ajax.php
+++ attrib_image_swap_module/1_Main_Files/ajax.php
@@ -2,23 +2,17 @@
 /**
  * ajax front controller
  *
- * @package core
- * @copyright Copyright 2003-2018 Zen Cart Development Team
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
+ * @copyright Copyright 2025 mc12345678.com McNumbers Ware
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: lat9 Fri Oct 26 10:04:06 2018 -0400 Modified in v1.5.6 $
+ * @version $Id: mc12345678 2025 Jun 25 Modified after v2.1.0 $
  */
-// Abort if the request was not an AJAX call
 if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
     http_response_code(400); // "Bad Request"
     exit();
 }
 
-// -----
-// Since this request can also be initiated from the admin-side's ajax.php, need
-// to ensure that we're bringing in the correct 'base' processing for the
-// rest of the initialization.
-//
 if (empty($zc_ajax_base_dir)) {
     $zc_ajax_base_dir = '';
 }
@@ -33,56 +27,45 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 
 
 // --- support functions ------------------
-if (!function_exists('utf8_encode_recurse')) {
-    function utf8_encode_recurse($mixed_value)
-    {
-        if (strtolower(CHARSET) == 'utf-8') {
-            return $mixed_value;
-        } elseif (!is_array($mixed_value)) {
-            return utf8_encode((string)$mixed_value);
-        } else {
-            $result = array();
-            foreach ($mixed_value as $key => $value) {
-                $result[$key] = utf8_encode($value);
-            }
-            return $result;
-        }
-    }
-}
-
 function ajaxAbort($status = 400, $msg = null)
 {
+    global $zc_ajax_base_dir;
     http_response_code($status); // 400 = "Bad Request"
-    if ($msg) echo $msg;
-    require('includes/application_bottom.php');
+    if ($msg) {
+        echo $msg;
+    }
+    require $zc_ajax_base_dir . 'includes/application_bottom.php';
     exit();
 }
 // --- support functions ------------------
 
 
 
-if (!isset($_GET['act']) || !isset($_GET['method'])) {
+if (!isset($_GET['act'], $_GET['method']) || !preg_match('/^[a-zA-Z0-9_]+$/', $_GET['act']) || !preg_match('/^[a-zA-Z0-9_]+$/', $_GET['method'])) {
     ajaxAbort();
 }
 
 $language_page_directory = DIR_WS_LANGUAGES . $_SESSION['language'] . '/';
 
 $className = 'zc' . ucfirst($_GET['act']);
-$classFile = $className . '.php';
-$basePath  = DIR_FS_CATALOG . DIR_WS_CLASSES;
-
-if (!file_exists(realpath($basePath . 'ajax/' . basename($classFile)))) {
-    ajaxAbort();
+$classFile = basename($className . '.php');
+$classPath = DIR_WS_CLASSES . 'ajax/';
+$basePath  = DIR_FS_CATALOG;
+$file = realpath($basePath . $classPath . $classFile);
+if (!empty($file) && file_exists($file)) {
+    require $file;
+} else {
+    $fs->loadFilesFromPluginsDirectory($installedPlugins, 'catalog/' . $classPath, '~^' . $classFile . '$~');
+    if (!class_exists($className)) {
+        ajaxAbort();
+    }
 }
 
-require realpath($basePath . 'ajax/' . basename($classFile));
 $class = new $className();
 if (!method_exists($class, $_GET['method'])) {
     ajaxAbort(400, 'class method error');
 }
 
-// Accepted request, so execute and return appropriate response:
 $result = call_user_func(array($class, $_GET['method']));
-$result = utf8_encode_recurse($result);
 echo json_encode($result);
-require('includes/application_bottom.php');
+require $zc_ajax_base_dir . 'includes/application_bottom.php';
